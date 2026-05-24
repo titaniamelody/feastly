6fd62fedfc896374c8efd4661ef3b022792a1397
Titania Melody <titaniamelody@gmail.com>
2026-05-24 23:10:04 +0500

    Update backend URL to new Back4App instance

--- frontend/src/utils/imageUrl.js
+++ frontend/src/utils/imageUrl.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = import.meta.env.VITE_API_URL || 'https://feastly-4e0nj1yx.b4a.run'
+export const BACKEND_URL = import.meta.env.VITE_API_URL || 'https://feastly-5o8wz9i0.b4a.run'
 
 /** DB may store "1777975092271food_1.png"; files on disk are food_1.png */
 export function resolveImageFileName(imageName) {
